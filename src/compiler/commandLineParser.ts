stevenfan/TypeScript

Add `--checkJsFiles`

src/compiler/commandLineParser.ts
@@ -458,6 +458,12 @@ namespace ts {
             name: "alwaysStrict",
             type: "boolean",
             description: Diagnostics.Parse_in_strict_mode_and_emit_use_strict_for_each_source_file
+        },
+        {
+            name: "checkJsFiles",
+            type: "boolean",
+            experimental: true,
+            description: Diagnostics.Show_errors_in_a_js_file
         }
     ];
 

openharmony/third_party_typescript

src/compiler/commandLineParser.ts
@@ -481,6 +481,12 @@ namespace ts {
                 name: "plugin",
                 type: "object"
             }
+        },
+        {
+            name: "checkJsFiles",
+            type: "boolean",
+            experimental: true,
+            description: Diagnostics.Report_errors_in_js_files
         }
     ];
 