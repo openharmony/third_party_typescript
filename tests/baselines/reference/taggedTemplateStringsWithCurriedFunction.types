openharmony/third_party_typescript

commit 45917c152bf8e0f27b6cd64fd63bb2eaea9dd1b2

add support for Lift Template Literal Restriction (#23801)
* add support for Lift Template Literal Restriction

* rename file and improve comment and tests

* fix NoSubstitutionTemplateLiteral support

* extract tagged template and add more test

* avoid useless parameter

* fix incorrect return node if cannot transform

* accept baseline

* correctly baseline

* accept baseline

* fix merge break

* fix merge break

* inline rescan template head or no subsititution template

* update scan error

* add comment and fix lint

* refactor and fix lint

* avoid blank

* fix merge conflict

* fix again

* fix again

* use multiple target

* fix space lint

Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

src/compiler/transformers/es2015.ts
@@ -3951,78 +3951,14 @@ namespace ts {
          * @param node A TaggedTemplateExpression node.
          */
         function visitTaggedTemplateExpression(node: TaggedTemplateExpression) {
-            // Visit the tag expression
-            const tag = visitNode(node.tag, visitor, isExpression);
-
-            // Build up the template arguments and the raw and cooked strings for the template.
-            // We start out with 'undefined' for the first argument and revisit later
-            // to avoid walking over the template string twice and shifting all our arguments over after the fact.
-            const templateArguments: Expression[] = [undefined!];
-            const cookedStrings: Expression[] = [];
-            const rawStrings: Expression[] = [];
-            const template = node.template;
-            if (isNoSubstitutionTemplateLiteral(template)) {
-                cookedStrings.push(createLiteral(template.text));
-                rawStrings.push(getRawLiteral(template));
-            }
-            else {
-                cookedStrings.push(createLiteral(template.head.text));
-                rawStrings.push(getRawLiteral(template.head));
-                for (const templateSpan of template.templateSpans) {
-                    cookedStrings.push(createLiteral(templateSpan.literal.text));
-                    rawStrings.push(getRawLiteral(templateSpan.literal));
-                    templateArguments.push(visitNode(templateSpan.expression, visitor, isExpression));
-                }
-            }
-
-            const helperCall = createTemplateObjectHelper(context, createArrayLiteral(cookedStrings), createArrayLiteral(rawStrings));
-
-            // Create a variable to cache the template object if we're in a module.
-            // Do not do this in the global scope, as any variable we currently generate could conflict with
-            // variables from outside of the current compilation. In the future, we can revisit this behavior.
-            if (isExternalModule(currentSourceFile)) {
-                const tempVar = createUniqueName("templateObject");
-                recordTaggedTemplateString(tempVar);
-                templateArguments[0] = createLogicalOr(
-                    tempVar,
-                    createAssignment(
-                        tempVar,
-                        helperCall)
-                );
-            }
-            else {
-                templateArguments[0] = helperCall;
-            }
-
-            return createCall(tag, /*typeArguments*/ undefined, templateArguments);
-        }
-
-        /**
-         * Creates an ES5 compatible literal from an ES6 template literal.
-         *
-         * @param node The ES6 template literal.
-         */
-        function getRawLiteral(node: TemplateLiteralLikeNode) {
-            // Find original source text, since we need to emit the raw strings of the tagged template.
-            // The raw strings contain the (escaped) strings of what the user wrote.
-            // Examples: `\n` is converted to "\\n", a template string with a newline to "\n".
-            let text = node.rawText;
-            if (text === undefined) {
-                text = getSourceTextOfNodeFromSourceFile(currentSourceFile, node);
-
-                // text contains the original source, it will also contain quotes ("`"), dolar signs and braces ("${" and "}"),
-                // thus we need to remove those characters.
-                // First template piece starts with "`", others with "}"
-                // Last template piece ends with "`", others with "${"
-                const isLast = node.kind === SyntaxKind.NoSubstitutionTemplateLiteral || node.kind === SyntaxKind.TemplateTail;
-                text = text.substring(1, text.length - (isLast ? 1 : 2));
-            }
-
-            // Newline normalization:
-            // ES6 Spec 11.8.6.1 - Static Semantics of TV's and TRV's
-            // <CR><LF> and <CR> LineTerminatorSequences are normalized to <LF> for both TV and TRV.
-            text = text.replace(/\r\n?/g, "\n");
-            return setTextRange(createLiteral(text), node);
+            return processTaggedTemplateExpression(
+                context,
+                node,
+                visitor,
+                currentSourceFile,
+                recordTaggedTemplateString,
+                ProcessLevel.All
+            );
         }
 
         /**
@@ -4358,18 +4294,6 @@ namespace ts {
         );
     }
 
-    function createTemplateObjectHelper(context: TransformationContext, cooked: ArrayLiteralExpression, raw: ArrayLiteralExpression) {
-        context.requestEmitHelper(templateObjectHelper);
-        return createCall(
-            getUnscopedHelperName("__makeTemplateObject"),
-            /*typeArguments*/ undefined,
-            [
-                cooked,
-                raw
-            ]
-        );
-    }
-
     export const extendsHelper: UnscopedEmitHelper = {
         name: "typescript:extends",
         importName: "__extends",
@@ -4391,17 +4315,4 @@ namespace ts {
                 };
             })();`
     };
-
-    export const templateObjectHelper: UnscopedEmitHelper = {
-        name: "typescript:makeTemplateObject",
-        importName: "__makeTemplateObject",
-        scoped: false,
-        priority: 0,
-        text: `
-            var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-                if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-                return cooked;
-            };`
-    };
-
 }

src/compiler/transformers/taggedTemplate.ts
@@ -1,0 +1,117 @@
+/*@internal*/
+namespace ts {
+    export enum ProcessLevel {
+        LiftRestriction,
+        All
+    }
+
+    export function processTaggedTemplateExpression(
+        context: TransformationContext,
+        node: TaggedTemplateExpression,
+        visitor: ((node: Node) => VisitResult<Node>) | undefined,
+        currentSourceFile: SourceFile,
+        recordTaggedTemplateString: (temp: Identifier) => void,
+        level: ProcessLevel) {
+
+        // Visit the tag expression
+        const tag = visitNode(node.tag, visitor, isExpression);
+
+        // Build up the template arguments and the raw and cooked strings for the template.
+        // We start out with 'undefined' for the first argument and revisit later
+        // to avoid walking over the template string twice and shifting all our arguments over after the fact.
+        const templateArguments: Expression[] = [undefined!];
+        const cookedStrings: Expression[] = [];
+        const rawStrings: Expression[] = [];
+        const template = node.template;
+
+        if (level === ProcessLevel.LiftRestriction && !hasInvalidEscape(template)) return node;
+
+        if (isNoSubstitutionTemplateLiteral(template)) {
+            cookedStrings.push(createTemplateCooked(template));
+            rawStrings.push(getRawLiteral(template, currentSourceFile));
+        }
+        else {
+            cookedStrings.push(createTemplateCooked(template.head));
+            rawStrings.push(getRawLiteral(template.head, currentSourceFile));
+            for (const templateSpan of template.templateSpans) {
+                cookedStrings.push(createTemplateCooked(templateSpan.literal));
+                rawStrings.push(getRawLiteral(templateSpan.literal, currentSourceFile));
+                templateArguments.push(visitNode(templateSpan.expression, visitor, isExpression));
+            }
+        }
+
+        const helperCall = createTemplateObjectHelper(context, createArrayLiteral(cookedStrings), createArrayLiteral(rawStrings));
+
+        // Create a variable to cache the template object if we're in a module.
+        // Do not do this in the global scope, as any variable we currently generate could conflict with
+        // variables from outside of the current compilation. In the future, we can revisit this behavior.
+        if (isExternalModule(currentSourceFile)) {
+            const tempVar = createUniqueName("templateObject");
+            recordTaggedTemplateString(tempVar);
+            templateArguments[0] = createLogicalOr(
+                tempVar,
+                createAssignment(
+                    tempVar,
+                    helperCall)
+            );
+        }
+        else {
+            templateArguments[0] = helperCall;
+        }
+
+        return createCall(tag, /*typeArguments*/ undefined, templateArguments);
+    }
+
+    function createTemplateCooked(template: TemplateHead | TemplateMiddle | TemplateTail | NoSubstitutionTemplateLiteral) {
+        return template.templateFlags ? createIdentifier("undefined") : createLiteral(template.text);
+    }
+
+    /**
+     * Creates an ES5 compatible literal from an ES6 template literal.
+     *
+     * @param node The ES6 template literal.
+     */
+    function getRawLiteral(node: LiteralLikeNode, currentSourceFile: SourceFile) {
+        // Find original source text, since we need to emit the raw strings of the tagged template.
+        // The raw strings contain the (escaped) strings of what the user wrote.
+        // Examples: `\n` is converted to "\\n", a template string with a newline to "\n".
+        let text = getSourceTextOfNodeFromSourceFile(currentSourceFile, node);
+
+        // text contains the original source, it will also contain quotes ("`"), dolar signs and braces ("${" and "}"),
+        // thus we need to remove those characters.
+        // First template piece starts with "`", others with "}"
+        // Last template piece ends with "`", others with "${"
+        const isLast = node.kind === SyntaxKind.NoSubstitutionTemplateLiteral || node.kind === SyntaxKind.TemplateTail;
+        text = text.substring(1, text.length - (isLast ? 1 : 2));
+
+        // Newline normalization:
+        // ES6 Spec 11.8.6.1 - Static Semantics of TV's and TRV's
+        // <CR><LF> and <CR> LineTerminatorSequences are normalized to <LF> for both TV and TRV.
+        text = text.replace(/\r\n?/g, "\n");
+        return setTextRange(createLiteral(text), node);
+    }
+
+    function createTemplateObjectHelper(context: TransformationContext, cooked: ArrayLiteralExpression, raw: ArrayLiteralExpression) {
+        context.requestEmitHelper(templateObjectHelper);
+        return createCall(
+            getUnscopedHelperName("__makeTemplateObject"),
+            /*typeArguments*/ undefined,
+            [
+                cooked,
+                raw
+            ]
+        );
+    }
+
+    export const templateObjectHelper: UnscopedEmitHelper = {
+        name: "typescript:makeTemplateObject",
+        importName: "__makeTemplateObject",
+        scoped: false,
+        priority: 0,
+        text: `
+            var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+                if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+                return cooked;
+            };`
+    };
+}

src/compiler/binder.ts
@@ -4124,8 +4124,18 @@ namespace ts {
             case SyntaxKind.TemplateHead:
             case SyntaxKind.TemplateMiddle:
             case SyntaxKind.TemplateTail:
-            case SyntaxKind.TemplateExpression:
+                if ((<NoSubstitutionTemplateLiteral | TemplateHead | TemplateMiddle | TemplateTail>node).templateFlags) {
+                    transformFlags |= TransformFlags.AssertES2018;
+                    break;
+                }
+                // falls through
             case SyntaxKind.TaggedTemplateExpression:
+                if (hasInvalidEscape((<TaggedTemplateExpression>node).template)) {
+                    transformFlags |= TransformFlags.AssertES2018;
+                    break;
+                }
+                // falls through
+            case SyntaxKind.TemplateExpression:
             case SyntaxKind.ShorthandPropertyAssignment:
             case SyntaxKind.StaticKeyword:
             case SyntaxKind.MetaProperty:

src/compiler/factoryPublic.ts
@@ -1486,7 +1486,7 @@ namespace ts {
 
         let token = rawTextScanner.scan();
         if (token === SyntaxKind.CloseBracketToken) {
-            token = rawTextScanner.reScanTemplateToken();
+            token = rawTextScanner.reScanTemplateToken(/* isTaggedTemplate */ false);
         }
 
         if (rawTextScanner.isUnterminated()) {

src/compiler/parser.ts
@@ -1137,8 +1137,12 @@ namespace ts {
             return currentToken = scanner.reScanSlashToken();
         }
 
-        function reScanTemplateToken(): SyntaxKind {
-            return currentToken = scanner.reScanTemplateToken();
+        function reScanTemplateToken(isTaggedTemplate: boolean): SyntaxKind {
+            return currentToken = scanner.reScanTemplateToken(isTaggedTemplate);
+        }
+
+        function reScanTemplateHeadOrNoSubstitutionTemplate(): SyntaxKind {
+            return currentToken = scanner.reScanTemplateHeadOrNoSubstitutionTemplate();
         }
 
         function reScanLessThanToken(): SyntaxKind {
@@ -2329,17 +2333,17 @@ namespace ts {
             return allowIdentifierNames ? parseIdentifierName() : parseIdentifier();
         }
 
-        function parseTemplateExpression(): TemplateExpression {
+        function parseTemplateExpression(isTaggedTemplate: boolean): TemplateExpression {
             const template = <TemplateExpression>createNode(SyntaxKind.TemplateExpression);
 
-            template.head = parseTemplateHead();
+            template.head = parseTemplateHead(isTaggedTemplate);
             Debug.assert(template.head.kind === SyntaxKind.TemplateHead, "Template head has wrong token kind");
 
             const list = [];
             const listPos = getNodePos();
 
             do {
-                list.push(parseTemplateSpan());
+                list.push(parseTemplateSpan(isTaggedTemplate));
             }
             while (last(list).literal.kind === SyntaxKind.TemplateMiddle);
 
@@ -2348,13 +2352,13 @@ namespace ts {
             return finishNode(template);
         }
 
-        function parseTemplateSpan(): TemplateSpan {
+        function parseTemplateSpan(isTaggedTemplate: boolean): TemplateSpan {
             const span = <TemplateSpan>createNode(SyntaxKind.TemplateSpan);
             span.expression = allowInAnd(parseExpression);
 
             let literal: TemplateMiddle | TemplateTail;
             if (token() === SyntaxKind.CloseBraceToken) {
-                reScanTemplateToken();
+                reScanTemplateToken(isTaggedTemplate);
                 literal = parseTemplateMiddleOrTemplateTail();
             }
             else {
@@ -2369,7 +2373,10 @@ namespace ts {
             return <LiteralExpression>parseLiteralLikeNode(token());
         }
 
-        function parseTemplateHead(): TemplateHead {
+        function parseTemplateHead(isTaggedTemplate: boolean): TemplateHead {
+            if (isTaggedTemplate) {
+                reScanTemplateHeadOrNoSubstitutionTemplate();
+            }
             const fragment = parseLiteralLikeNode(token());
             Debug.assert(fragment.kind === SyntaxKind.TemplateHead, "Template head has wrong token kind");
             return <TemplateHead>fragment;
@@ -2411,6 +2418,10 @@ namespace ts {
             // parent unary expression.
             if (node.kind === SyntaxKind.NumericLiteral) {
                 (<NumericLiteral>node).numericLiteralFlags = scanner.getTokenFlags() & TokenFlags.NumericLiteralFlags;
+            }
+
+            if (isTemplateLiteralKind(node.kind)) {
+                (<TemplateHead | TemplateMiddle | TemplateTail | NoSubstitutionTemplateLiteral>node).templateFlags = scanner.getTokenFlags() & TokenFlags.ContainsInvalidEscape;
             }
 
             nextToken();
@@ -4772,8 +4783,8 @@ namespace ts {
             tagExpression.questionDotToken = questionDotToken;
             tagExpression.typeArguments = typeArguments;
             tagExpression.template = token() === SyntaxKind.NoSubstitutionTemplateLiteral
-                ? <NoSubstitutionTemplateLiteral>parseLiteralNode()
-                : parseTemplateExpression();
+                ? (reScanTemplateHeadOrNoSubstitutionTemplate(), <NoSubstitutionTemplateLiteral>parseLiteralNode())
+                : parseTemplateExpression(/*isTaggedTemplate*/ true);
             if (questionDotToken || tag.flags & NodeFlags.OptionalChain) {
                 tagExpression.flags |= NodeFlags.OptionalChain;
             }
@@ -4945,7 +4956,7 @@ namespace ts {
                     }
                     break;
                 case SyntaxKind.TemplateHead:
-                    return parseTemplateExpression();
+                    return parseTemplateExpression(/* isTaggedTemplate */ false);
             }
 
             return parseIdentifier(Diagnostics.Expression_expected);

commit af78fd67394324563985270020f58cc5b3ca7b1e

Make `processTaggedTemplateExpression` visit a returned node
This problem was introduced in 45917c152bf (from PR #23801), which added
a `visitTaggedTemplateExpression` case for `TaggedTemplateExpression`,
before that, it would fallback to the default of `visitNode`.  So re-add
that happen in `processTaggedTemplateExpression`.

Since it doesn't hurt, I left a `Debug.checkDefined(property.name)`
instead of `!`-ing it.

Fixes #38558.

src/compiler/transformers/es2015.ts
@@ -2583,7 +2583,7 @@ namespace ts {
                     && i < numInitialPropertiesWithoutYield) {
                     numInitialPropertiesWithoutYield = i;
                 }
-                if (property.name!.kind === SyntaxKind.ComputedPropertyName) {
+                if (Debug.checkDefined(property.name).kind === SyntaxKind.ComputedPropertyName) {
                     numInitialProperties = i;
                     break;
                 }

src/compiler/transformers/taggedTemplate.ts
@@ -8,7 +8,7 @@ namespace ts {
     export function processTaggedTemplateExpression(
         context: TransformationContext,
         node: TaggedTemplateExpression,
-        visitor: ((node: Node) => VisitResult<Node>) | undefined,
+        visitor: Visitor,
         currentSourceFile: SourceFile,
         recordTaggedTemplateString: (temp: Identifier) => void,
         level: ProcessLevel) {
@@ -24,7 +24,9 @@ namespace ts {
         const rawStrings: Expression[] = [];
         const template = node.template;
 
-        if (level === ProcessLevel.LiftRestriction && !hasInvalidEscape(template)) return node;
+        if (level === ProcessLevel.LiftRestriction && !hasInvalidEscape(template)) {
+            return visitEachChild(node, visitor, context);
+        }
 
         if (isNoSubstitutionTemplateLiteral(template)) {
             cookedStrings.push(createTemplateCooked(template));

tests/baselines/reference/taggedTemplateStringsWithCurriedFunction.js
@@ -1,0 +1,41 @@
+//// [taggedTemplateStringsWithCurriedFunction.ts]
+// Originated from #38558
+
+const f = _ => (..._) => "";
+
+f({ ...{ x: 0 } })``;
+f({ ...{ x: 0 } })`x`;
+f({ ...{ x: 0 } })`x${f}x`;
+f({ ...{ x: 0 }, y: (() => 1)() })``;
+f({ x: (() => 1)(), ...{ y: 1 } })``;
+
+
+//// [taggedTemplateStringsWithCurriedFunction.js]
+// Originated from #38558
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var f = function (_) { return function () {
+    var _ = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        _[_i] = arguments[_i];
+    }
+    return "";
+}; };
+f(__assign({ x: 0 }))(__makeTemplateObject([""], [""]));
+f(__assign({ x: 0 }))(__makeTemplateObject(["x"], ["x"]));
+f(__assign({ x: 0 }))(__makeTemplateObject(["x", "x"], ["x", "x"]), f);
+f(__assign({ x: 0 }, { y: (function () { return 1; })() }))(__makeTemplateObject([""], [""]));
+f(__assign({ x: (function () { return 1; })() }, { y: 1 }))(__makeTemplateObject([""], [""]));

tests/baselines/reference/taggedTemplateStringsWithCurriedFunction.symbols
@@ -1,0 +1,31 @@
+=== tests/cases/compiler/taggedTemplateStringsWithCurriedFunction.ts ===
+// Originated from #38558
+
+const f = _ => (..._) => "";
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>_ : Symbol(_, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 9))
+>_ : Symbol(_, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 16))
+
+f({ ...{ x: 0 } })``;
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>x : Symbol(x, Decl(taggedTemplateStringsWithCurriedFunction.ts, 4, 8))
+
+f({ ...{ x: 0 } })`x`;
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>x : Symbol(x, Decl(taggedTemplateStringsWithCurriedFunction.ts, 5, 8))
+
+f({ ...{ x: 0 } })`x${f}x`;
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>x : Symbol(x, Decl(taggedTemplateStringsWithCurriedFunction.ts, 6, 8))
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+
+f({ ...{ x: 0 }, y: (() => 1)() })``;
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>x : Symbol(x, Decl(taggedTemplateStringsWithCurriedFunction.ts, 7, 8))
+>y : Symbol(y, Decl(taggedTemplateStringsWithCurriedFunction.ts, 7, 16))
+
+f({ x: (() => 1)(), ...{ y: 1 } })``;
+>f : Symbol(f, Decl(taggedTemplateStringsWithCurriedFunction.ts, 2, 5))
+>x : Symbol(x, Decl(taggedTemplateStringsWithCurriedFunction.ts, 8, 3))
+>y : Symbol(y, Decl(taggedTemplateStringsWithCurriedFunction.ts, 8, 24))
+

tests/baselines/reference/taggedTemplateStringsWithCurriedFunction.types
@@ -1,0 +1,72 @@
+=== tests/cases/compiler/taggedTemplateStringsWithCurriedFunction.ts ===
+// Originated from #38558
+
+const f = _ => (..._) => "";
+>f : (_: any) => (..._: any[]) => string
+>_ => (..._) => "" : (_: any) => (..._: any[]) => string
+>_ : any
+>(..._) => "" : (..._: any[]) => string
+>_ : any[]
+>"" : ""
+
+f({ ...{ x: 0 } })``;
+>f({ ...{ x: 0 } })`` : string
+>f({ ...{ x: 0 } }) : (..._: any[]) => string
+>f : (_: any) => (..._: any[]) => string
+>{ ...{ x: 0 } } : { x: number; }
+>{ x: 0 } : { x: number; }
+>x : number
+>0 : 0
+>`` : ""
+
+f({ ...{ x: 0 } })`x`;
+>f({ ...{ x: 0 } })`x` : string
+>f({ ...{ x: 0 } }) : (..._: any[]) => string
+>f : (_: any) => (..._: any[]) => string
+>{ ...{ x: 0 } } : { x: number; }
+>{ x: 0 } : { x: number; }
+>x : number
+>0 : 0
+>`x` : "x"
+
+f({ ...{ x: 0 } })`x${f}x`;
+>f({ ...{ x: 0 } })`x${f}x` : string
+>f({ ...{ x: 0 } }) : (..._: any[]) => string
+>f : (_: any) => (..._: any[]) => string
+>{ ...{ x: 0 } } : { x: number; }
+>{ x: 0 } : { x: number; }
+>x : number
+>0 : 0
+>`x${f}x` : string
+>f : (_: any) => (..._: any[]) => string
+
+f({ ...{ x: 0 }, y: (() => 1)() })``;
+>f({ ...{ x: 0 }, y: (() => 1)() })`` : string
+>f({ ...{ x: 0 }, y: (() => 1)() }) : (..._: any[]) => string
+>f : (_: any) => (..._: any[]) => string
+>{ ...{ x: 0 }, y: (() => 1)() } : { y: number; x: number; }
+>{ x: 0 } : { x: number; }
+>x : number
+>0 : 0
+>y : number
+>(() => 1)() : number
+>(() => 1) : () => number
+>() => 1 : () => number
+>1 : 1
+>`` : ""
+
+f({ x: (() => 1)(), ...{ y: 1 } })``;
+>f({ x: (() => 1)(), ...{ y: 1 } })`` : string
+>f({ x: (() => 1)(), ...{ y: 1 } }) : (..._: any[]) => string
+>f : (_: any) => (..._: any[]) => string
+>{ x: (() => 1)(), ...{ y: 1 } } : { y: number; x: number; }
+>x : number
+>(() => 1)() : number
+>(() => 1) : () => number
+>() => 1 : () => number
+>1 : 1
+>{ y: 1 } : { y: number; }
+>y : number
+>1 : 1
+>`` : ""
+

tests/cases/compiler/taggedTemplateStringsWithCurriedFunction.ts
@@ -1,0 +1,11 @@
+//@target: es3
+
+// Originated from #38558
+
+const f = _ => (..._) => "";
+
+f({ ...{ x: 0 } })``;
+f({ ...{ x: 0 } })`x`;
+f({ ...{ x: 0 } })`x${f}x`;
+f({ ...{ x: 0 }, y: (() => 1)() })``;
+f({ x: (() => 1)(), ...{ y: 1 } })``;