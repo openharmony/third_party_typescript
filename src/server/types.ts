7577989e3e0110425d527018cbaa2f034cfc8de7
Guangyao Ma <maguangyao@huawei.com>
2023-02-14 19:34:27 +0800

    fix: fix up eslint & bugs buglist: 1. modify getTagNameNeededCheckByFile type definition 2. add sourceSymbol.declarations[0].Inherit to inherited parent node's JsDoc 3. historical test failures
    Signed-off-by: Guangyao Ma <maguangyao@huawei.com>
    Change-Id: I49f7ba39c7b3ba395686390c9b37727a1fd37878

--- src/server/types.ts
+++ src/server/types.ts
@@ -16,7 +16,7 @@ declare namespace ts.server {
         gc?(): void;
         trace?(s: string): void;
         require?(initialPath: string, moduleName: string): RequireResult;
-        getTagNameNeededCheckByFile?(filePath: string): TagCheckParam;
+        getTagNameNeededCheckByFile?(containFilePath: string, sourceFilePath: string): TagCheckParam;
         getExpressionCheckedResultsByFile?(filePath: string, jsDocs: JSDoc[]): ConditionCheckResult;
     }
 }
