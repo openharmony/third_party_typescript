3de36e5ca7e94a56cc7352bc1e799781f953af1e
zhangchen168 <zhangchen168@huawei.com>
2023-02-25 17:06:07 +0800

    Support json5 format files parsing
    Issue: https://gitee.com/openharmony/third_party_typescript/issues/I6INQY
    Test: tsc testsuite
    
    Signed-off-by: zhangchen168 <zhangchen168@huawei.com>
    Change-Id: Ic1091be411d3d0479615afeaa2895f2e20c0928a

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -4759,6 +4759,7 @@ declare namespace ts {
     };
     export {};
 }
+declare const JSON5: any;
 declare namespace ts {
     function getEffectiveTypeRoots(options: CompilerOptions, host: GetEffectiveTypeRootsHost): string[] | undefined;
     /**
