1cae5be352091f9e0df301d6f16a2d1e0fa06b01
hid20141227 <yangrui185@huawei.com>
2024-05-15 17:39:40 +0800

    Relax BitVector indexed access
    issue: I9PKL0
    
    Test: arkts test
    
    Signed-off-by: hid20141227 <yangrui185@huawei.com>
    Change-Id: Ibd6097546d2c13f574c16cd3c57e6892d511a8c1

--- lib/typescriptServices.d.ts
+++ lib/typescriptServices.d.ts
@@ -9234,7 +9234,6 @@ declare namespace ts {
             const SENDABLE_INTERFACE = "ISendable";
             const ARKTS_COLLECTIONS_D_ETS = "@arkts.collections.d.ets";
             const COLLECTIONS_NAMESPACE = "collections";
-            const ARKTS_COLLECTIONS_TYPES: string[];
             const ARKTS_LANG_D_ETS = "@arkts.lang.d.ets";
             const LANG_NAMESPACE = "lang";
             const ISENDABLE_TYPE = "ISendable";
