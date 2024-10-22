61c166c380fc7db62cd130e800305eb6c1736416
zju_wyx <wangyunxiao8@huawei.com>
2024-10-22 14:27:21 +0800

    memory optimization
    Issue: IBGF0S
    Signed-off-by: zju_wyx <wangyunxiao8@huawei.com>
    Change-Id: Ia02f0aaf6ec73302e5f4fd22f0bd0ddfa6394756

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -4588,6 +4588,12 @@ namespace ts {
          */
         releaseTypeChecker(): void;
         getEmitHost(writeFileCallback?: WriteFileCallback): EmitHost;
+        refreshTypeChecker(): void;
+        setProgramSourceFiles(file: SourceFile): void;
+        initProcessingFiles(): void;
+        processImportedModules(file: SourceFile): void;
+        getProcessingFiles(): SourceFile[] | undefined;
+        deleteProgramSourceFiles(fileNames: string[]): void;
     }
 
     /*@internal*/
