fc1a2fa341aab416e73f76234783ac7d47cf48a8
xiaopeiyang <xiaopeiyang1@huawei.com>
2025-04-02 14:15:25 +0800

    Fix bug of Resource
    Issue: https://gitee.com/openharmony/third_party_typescript/issues/IBY8XV
    
    Signed-off-by: xiaopeiyang <xiaopeiyang1@huawei.com>
    Change-Id: I80564e9272db83fd34f74e3639393c102386bcbe

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6857,7 +6857,7 @@ export function createTypeChecker(host: TypeCheckerHost, isTypeCheckerForLinter:
                     }
                 }
                 // Only for Resource type in openharmony SDK
-                if (specifier.endsWith('/openharmony/ets/api/global/resource')) {
+                if (specifier.endsWith('/openharmony/ets/ets1.1/api/global/resource')) {
                     const lastSymbol = chain[chain.length - 1];
                     // Create typeReferenceNode if is Resource type
                     if (lastSymbol.escapedName === 'Resource') {
