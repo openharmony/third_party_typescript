d59cc735712bc70b8394f789ab25b25590042d8f
liuqian <liuqian227@huawei.com>
2025-08-07 16:02:09 +0800

    1.1 references a class with a constructor in 1.2
    Issue: https://gitee.com/openharmony/third_party_typescript/issues/ICRUMB
    Signed-off-by: liuqian <liuqian227@huawei.com>
    Change-Id: I20de1c6a8afe011a608e63b3d9f4df474aeeab7a

---
+++ tests/arkTSTest/testcase/arkts-no-class-literal-contructor/lib-dependency.ts
@@ -0,0 +1,38 @@
+/*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// ts as a library (har)
+export class C12 {
+  b:number;
+
+  constructor(num1:number) {
+    this.b = num1;
+  }
+}
+
+export function isC12(o: C12) {
+  return o instanceof C12;
+}
+
+export class B12 {
+  b:number=1;
+
+  constructor() {
+  }
+}
+
+export function isB12(o: B12) {
+  return o instanceof B12;
+}
