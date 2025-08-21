e55660cacadb261a336cd203db8491b3b3cd8a41
x30053363 <xiaopeiyang1@huawei.com>
2025-08-21 21:17:49 +0800

    Fix Linter incremental build
    Issue: https://gitee.com/openharmony/third_party_typescript/issues/ICU4TS
    
    Signed-off-by: x30053363 <xiaopeiyang1@huawei.com>
    Change-Id: Ie225e0cc46f29054f22c0e742a69f2fb0631bdee

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -986,7 +986,7 @@ export function createTypeChecker(host: TypeCheckerHost, isTypeCheckerForLinter:
         deleteConstEnumRelate: (path: string) => {constEnumRelate && constEnumRelate.delete(path)},
         getTypeArgumentsForResolvedSignature,
         getCheckedSourceFiles: () => checkedSourceFiles,
-        collectHaveTsNoCheckFilesForLinter: (sourceFile: SourceFile) => {isTypeCheckerForLinter && checkedSourceFiles.add(sourceFile)},
+        collectHaveTsNoCheckFilesForLinter: (sourceFile: SourceFile) => {checkedSourceFiles.add(sourceFile)},
         clearQualifiedNameCache: () => {qualifiedNameCache && qualifiedNameCache.clear()},
         isStaticRecord: isStaticRecord,
         isStaticSourceFile: isStaticSourceFile,
@@ -44311,10 +44311,9 @@ export function createTypeChecker(host: TypeCheckerHost, isTypeCheckerForLinter:
                 clear(potentialReflectCollisions);
             }
 
-            // collecting incremental affected files when the isTypeCheckerForLinter is true
-            if (isTypeCheckerForLinter) {
-                checkedSourceFiles.add(node);
-            }
+            // collecting incremental affected files
+            checkedSourceFiles.add(node);
+
             links.flags |= NodeCheckFlags.TypeChecked;
         }
     }
